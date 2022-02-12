92363b843131c4ac0594e45818d87e667bb344da
harishreyya <harishrebel0@gmail.com>
2022-02-12 22:30:36 +0530

    jongnem

---
+++ oj.js
@@ -0,0 +1,31 @@
+
+function runProgram(input) {
+   
+    
+}
+ 
+
+   if (process.env.USERNAME === 'haris') {
+     runProgram(`3 7
+      0 0 0 0 1 1 1
+0 0 1 1 1 1 1
+0 0 0 0 0 0 0 `)
+    } else {
+     process.stdin.resume();
+     process.stdin.setEncoding("ascii");
+     let read = "";
+     process.stdin.on("data", function (input) {
+       read += input;
+     });
+     process.stdin.on("end", function () {
+       read = read.replace(/\n$/, "");
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+     });
+     process.on("SIGINT", function () {
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+       process.exit(0);
+     });
+   }
+   

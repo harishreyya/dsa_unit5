f653eb8ca4d7fbcab1e0d63e218b0a5dfd89e7af
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-03-05 23:46:18 +0530

    practice

--- ne.js
+++ ne.js
@@ -3,14 +3,27 @@ function runProgram(input) {
    
     input  = input.trim().split("\n");
     let test  = +input[0];
-    let 
+    let line = 1;
+    for(i=0;i<test;i++){
+        let size = +input[line++];
+        let arr = input[line++].trim().split(" ").map(Number);
+        for(i=0;i<arr.length;i++){
+    arr[i]+= arr[i]
+    console.log(arr[i])
+        }
+       console.log(size,arr) 
+    }
 
     
 }
  
 
    if (process.env.USERNAME === 'haris') {
-     runProgram(``)
+     runProgram(`2
+     5
+     2 3 5 4 7
+     3
+     2 4 5`)
     } else {
      process.stdin.resume();
      process.stdin.setEncoding("ascii");

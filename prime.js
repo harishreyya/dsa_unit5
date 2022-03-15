ff5280af1f90865cf3daadba2096d423dc111d76
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-03-15 20:00:20 +0530

    practice

---
+++ prime.js
@@ -0,0 +1,44 @@
+
+function runProgram(input) {
+   input = +input[0]
+   //console.log(input)
+   let count  = 0;
+   for(i=0;i<Math.sqrt(input);i++){
+       if(input%i==0){
+           if(i%i!=input){
+      count = count + 2
+           }else{
+               count = count + 1
+           }
+       }
+   } if( count == 2 )
+ console.log(  "Given number prime");
+   
+   else
+  console.log( "Given number is not prime");
+
+    
+}
+ 
+
+   if (process.env.USERNAME === 'haris') {
+     runProgram(`4`)
+    } else {
+     process.stdin.resume();
+     process.stdin.setEncoding("ascii");
+     let read = "";
+     process.stdin.on("data", function (input) {
+       read += input;
+     });
+     process.stdin.on("end", function () {
+       read = read.replace(/\n$/, "");
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+     });
+     process.on("SIGINT", function () {
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+       process.exit(0);
+     });
+   }
+   

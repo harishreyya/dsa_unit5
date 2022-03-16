eea0de0f8647bc727bee2263dcd2436fc927d3a9
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-03-16 21:00:09 +0530

    practice

---
+++ bubblesort.js
@@ -0,0 +1,41 @@
+
+function runProgram(input) {
+   input = input.trim().split(" ").map(Number);
+  // int size //size of the array
+ let  size = input.length
+	//input Array
+	for ( i=0;i<size - 1;i++){
+	    for ( j=0;j<size - i - 1;j++){
+		if (input[j] > input[j + 1]){ 
+     
+        input[j] = input[j+1]
+        input[j+1] = input[j]
+        }
+        }  }  console.log(input) 
+    
+
+    
+}
+ 
+
+   if (process.env.USERNAME === 'haris') {
+     runProgram(`4 2 3 5 1 6 9`)
+    } else {
+     process.stdin.resume();
+     process.stdin.setEncoding("ascii");
+     let read = "";
+     process.stdin.on("data", function (input) {
+       read += input;
+     });
+     process.stdin.on("end", function () {
+       read = read.replace(/\n$/, "");
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+     });
+     process.on("SIGINT", function () {
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+       process.exit(0);
+     });
+   }
+   

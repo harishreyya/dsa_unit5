538527c69dbc2d0cc78618cdfeb38789b670daa8
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-03-19 16:31:51 +0530

    practixe quiz

--- quick.js
+++ quick.js
@@ -1,30 +1,178 @@
+// function alpha(){
+//   var name = "harish"
+//   console.log(this.name)
+// }
+// var name = "masai_school"
+// alpha()
 
-function runProgram(input) {
-   
-    
+// var x = 100;
+// function alpha(){
+//   let x = 200
+//   function beta(){
+//     console.log(x);
 
-    
+//   }
+//   return beta
+// }
+
+// var gamma = alpha();
+// gamma()
+
+// // function value(b){
+// //   const a = 100;
+// //   a = b
+// //   return a
+// // }
+// // console.log(value(100))
+
+// // const arr = [10,20,15,21];
+// // for(var i =  0;i<arr.length;i++){
+// //   setInterval(function(){
+// //     console.log("index"+i+"elemnt"+arr[i])
+// //   },3000)
+// // }
+
+
+
+// // var printnam = function(name){
+// //   console.log("helo",name)
+// // }
+// // printnam("sonil")
+
+// // var str = "masai";
+// // var {length} = str
+
+// // // console.log(length)
+
+// // var arr = new Array(5).fill(0).map(function(a,b){return b+1})
+// // console.log(arr)
+
+// // function getYear(){
+// //   var year = new Date().getFullYear()
+// //   return year
+// // }
+
+// // console.log(getYear())
+
+// // // console.log(year)
+// "use strict"
+// function name(){
+//   var name = "masai"
+//   console.log(this.name)
+// }
+// function greet(){
+//   console.log("hello")
+
+// }
+
+// var arr = [{name:"nrupul"},{name:"aman"},{name:"albert"}]
+
+// for(var i=0; i<arr.length;i++){
+//   setTimeout(name.call(arr[i]),0)
+//   setTimeout(greet(),0)
+// // }
+// // foo()
+
+// // function foo(){
+// //   console.log("sinu")
+// // }
+// // console.log(name)
+// // name = "masai"
+
+// // for(var i = 0; i<=5;i++){
+// //     setTimeout(function asyncronous(){
+// //       console.log(i)
+// //     },i*1000)
+// //   }
+// // let fname = "masi"
+// // let lnmae = "schol"
+
+// // const person = {fname,lnmae}
+
+// // // console.log(person)
+// // console.log([1]===[1])
+// var name = "ajay";
+// function printdetails(){
+//   console.log(this.name)
+// }
+
+// var person = {
+//   name:"harem",
+//   company:"masai",
+//   print : printdetails()
+// }
+
+// person.print()
+
+
+
+
+
+// // arr = ["a","b","c","d","e"];
+// // const func = ([f,...r]) => r.reduce((a,c)=>a+c)+f
+// // console.log(func(a
+// function alpha(){
+//   var x = 100;
+//   this.beta()
+// }
+
+// function beta(){
+// //   var x = 200
+// // //   console.log(this.x)
+// // // }
+
+// // // alpha()
+
+// // // kat = "nasai"
+
+
+// // // console.log(kat)
+
+// getNmae = ()=>{
+//   var status = true;
+//   if(status){
+//     var name = "masai"
+//   }
+
+//   console.log(name)
+// }
+
+// getNmae()
+
+
+// // const sum = (x,y)=>{
+// //   x=x || 5
+// //   y = y|| 10
+
+
+// //   return x+y
+// // }
+
+// // console.log(sum(0,10))
+
+// cont = (x)=>{
+// var y = x*10
+
+// val=(z)=>{
+//   console.log(x,y,z)
+// }
+
+// val(y*5)
+// }
+
+// cont (3)
+
+// console.log(name);
+// name="masai"
+
+// var name;
+
+pritname("shonil");
+var pritname
+pritname=(name)=>{
+  console.log("hello",name)
 }
- 
 
-   if (process.env.USERNAME === 'haris') {
-     runProgram(``)
-    } else {
-     process.stdin.resume();
-     process.stdin.setEncoding("ascii");
-     let read = "";
-     process.stdin.on("data", function (input) {
-       read += input;
-     });
-     process.stdin.on("end", function () {
-       read = read.replace(/\n$/, "");
-       read = read.replace(/\n$/, "");
-       runProgram(read);
-     });
-     process.on("SIGINT", function () {
-       read = read.replace(/\n$/, "");
-       runProgram(read);
-       process.exit(0);
-     });
-   }
-   
+pritname = function(name){
+  console.log("hey",name)
+}

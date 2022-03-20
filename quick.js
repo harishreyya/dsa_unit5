ecf9903c8376cade7e4655184a19b7fbcae1cd0a
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-03-20 12:48:25 +0530

    prACTICE

--- quick.js
+++ quick.js
@@ -18,43 +18,43 @@
 // var gamma = alpha();
 // gamma()
 
-// // function value(b){
-// //   const a = 100;
-// //   a = b
-// //   return a
-// // }
-// // console.log(value(100))
+// function value(b){
+//   const a = 100;
+//   a = b
+//   return a
+// }
+// console.log(value(100))
 
-// // const arr = [10,20,15,21];
-// // for(var i =  0;i<arr.length;i++){
-// //   setInterval(function(){
-// //     console.log("index"+i+"elemnt"+arr[i])
-// //   },3000)
-// // }
+// const arr = [10,20,15,21];
+// for(var i =  0;i<arr.length;i++){
+//   setInterval(function(){
+//     console.log("index"+i+"elemnt"+arr[i])
+//   },3000)
+// }
 
 
 
-// // var printnam = function(name){
-// //   console.log("helo",name)
-// // }
-// // printnam("sonil")
+// var printnam = function(name){
+//   console.log("helo",name)
+// }
+// printnam("sonil")
 
-// // var str = "masai";
-// // var {length} = str
+// var str = "masai";
+// var {length} = str
 
-// // // console.log(length)
+// // console.log(length)
 
-// // var arr = new Array(5).fill(0).map(function(a,b){return b+1})
-// // console.log(arr)
+// var arr = new Array(5).fill(0).map(function(a,b){return b+1})
+// console.log(arr)
 
-// // function getYear(){
-// //   var year = new Date().getFullYear()
-// //   return year
-// // }
+// function getYear(){
+//   var year = new Date().getFullYear()
+//   return year
+// }
 
-// // console.log(getYear())
+// console.log(getYear())
 
-// // // console.log(year)
+// // console.log(year)
 // "use strict"
 // function name(){
 //   var name = "masai"
@@ -71,108 +71,108 @@
 //   setTimeout(name.call(arr[i]),0)
 //   setTimeout(greet(),0)
 // // }
-// // foo()
+// // // foo()
 
-// // function foo(){
-// //   console.log("sinu")
+// // // function foo(){
+// // //   console.log("sinu")
+// // // }
+// // // console.log(name)
+// // // name = "masai"
+
+// // // for(var i = 0; i<=5;i++){
+// // //     setTimeout(function asyncronous(){
+// // //       console.log(i)
+// // //     },i*1000)
+// // //   }
+// // // let fname = "masi"
+// // // let lnmae = "schol"
+
+// // // const person = {fname,lnmae}
+
+// // // // console.log(person)
+// // // console.log([1]===[1])
+// // var name = "ajay";
+// // function printdetails(){
+// //   console.log(this.name)
 // // }
-// // console.log(name)
-// // name = "masai"
 
-// // for(var i = 0; i<=5;i++){
-// //     setTimeout(function asyncronous(){
-// //       console.log(i)
-// //     },i*1000)
+// // var person = {
+// //   name:"harem",
+// //   company:"masai",
+// //   print : printdetails()
+// // }
+
+// // person.print()
+
+
+
+
+
+// // // arr = ["a","b","c","d","e"];
+// // // const func = ([f,...r]) => r.reduce((a,c)=>a+c)+f
+// // // console.log(func(a
+// // function alpha(){
+// //   var x = 100;
+// //   this.beta()
+// // }
+
+// // function beta(){
+// // //   var x = 200
+// // // //   console.log(this.x)
+// // // // }
+
+// // // // alpha()
+
+// // // // kat = "nasai"
+
+
+// // // // console.log(kat)
+
+// // getNmae = ()=>{
+// //   var status = true;
+// //   if(status){
+// //     var name = "masai"
 // //   }
-// // let fname = "masi"
-// // let lnmae = "schol"
 
-// // const person = {fname,lnmae}
+// //   console.log(name)
+// // }
 
-// // // console.log(person)
-// // console.log([1]===[1])
-// var name = "ajay";
-// function printdetails(){
-//   console.log(this.name)
-// }
-
-// var person = {
-//   name:"harem",
-//   company:"masai",
-//   print : printdetails()
-// }
-
-// person.print()
+// // getNmae()
 
 
+// // // const sum = (x,y)=>{
+// // //   x=x || 5
+// // //   y = y|| 10
 
 
-
-// // arr = ["a","b","c","d","e"];
-// // const func = ([f,...r]) => r.reduce((a,c)=>a+c)+f
-// // console.log(func(a
-// function alpha(){
-//   var x = 100;
-//   this.beta()
-// }
-
-// function beta(){
-// //   var x = 200
-// // //   console.log(this.x)
+// // //   return x+y
 // // // }
 
-// // // alpha()
+// // // console.log(sum(0,10))
 
-// // // kat = "nasai"
+// // cont = (x)=>{
+// // var y = x*10
 
-
-// // // console.log(kat)
-
-// getNmae = ()=>{
-//   var status = true;
-//   if(status){
-//     var name = "masai"
-//   }
-
-//   console.log(name)
-// }
-
-// getNmae()
-
-
-// // const sum = (x,y)=>{
-// //   x=x || 5
-// //   y = y|| 10
-
-
-// //   return x+y
+// // val=(z)=>{
+// //   console.log(x,y,z)
 // // }
 
-// // console.log(sum(0,10))
+// // val(y*5)
+// // }
 
-// cont = (x)=>{
-// var y = x*10
+// // cont (3)
 
-// val=(z)=>{
-//   console.log(x,y,z)
+// // console.log(name);
+// // name="masai"
+
+// // var name;
+
+// pritname("shonil");
+// var pritname
+// pritname=(name)=>{
+//   console.log("hello",name)
 // }
 
-// val(y*5)
+// pritname = function(name){
+//   console.log("hey",name)
 // }
-
-// cont (3)
-
-// console.log(name);
-// name="masai"
-
-// var name;
-
-pritname("shonil");
-var pritname
-pritname=(name)=>{
-  console.log("hello",name)
-}
-
-pritname = function(name){
-  console.log("hey",name)
-}

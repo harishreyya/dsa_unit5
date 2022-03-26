dc939a92b920fb40ccfb706d40dad5ebb2885fd9
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-03-26 18:27:31 +0530

    oshs

---
+++ strequal.js
@@ -0,0 +1,31 @@
+
+function runProgram(input) {
+   let str = input.trim().split("");
+  // console.log(str)
+    
+
+    
+}
+ 
+
+   if (process.env.USERNAME === 'haris') {
+     runProgram(`23=5`)
+    } else {
+     process.stdin.resume();
+     process.stdin.setEncoding("ascii");
+     let read = "";
+     process.stdin.on("data", function (input) {
+       read += input;
+     });
+     process.stdin.on("end", function () {
+       read = read.replace(/\n$/, "");
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+     });
+     process.on("SIGINT", function () {
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+       process.exit(0);
+     });
+   }
+   
--- test.js
+++ test.js
@@ -1,3 +1,16 @@
-let a = 4;
-let b = 8;
-console.log(a^b)
+
+
+var x = 7
+// console.log(x)
+
+function getname(){
+    console.log("aur bhai hoisting")
+    
+}
+
+
+console.log(getname())
+
+/*hoisting is a phenomenon in javascript by
+ which you can access the functions and
+  variables even before you have initialzied it.*/

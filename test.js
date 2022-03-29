0d11de13d7d809f67dbcaaa8656964c13c71c029
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-03-29 08:58:09 +0530

    practice

--- test.js
+++ test.js
@@ -84,25 +84,28 @@
 
 //------------------------------------------------------------------------------
 
-function foo1(){
-    return {
-        bar:'hello'
-    };
-}
-function foo2(){
-    return 
-    {
-        bar:"hello"
-    };
-}
-function foo3(){
-    return {
-        bar:"hello"
-    };
-}
-foo1()
-foo2()
-foo3()
-// console.log(foo1())
-// console.log(foo2())
-// console.log(foo3())
+// function foo1(){
+//     return {
+//         bar:'hello'
+//     };
+// }
+// function foo2(){
+//     return 
+//     {
+//         bar:"hello"
+//     };
+// }
+// function foo3(){
+//     return {
+//         bar:"hello"
+//     };
+// }
+// foo1()
+// foo2()
+// foo3()
+// // console.log(foo1())
+// // console.log(foo2())
+// // console.log(foo3())
+
+
+// ----------------------------------------------------------------------------------

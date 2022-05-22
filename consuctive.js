3109d6aec8a9c40abc188e41540b1d3eb320a740
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-05-22 22:55:50 +0530

    srg

--- consuctive.js
+++ consuctive.js
@@ -1,39 +1,43 @@
 
-function runProgram(input) {
-   input = input.split(" ")
+// function runProgram(input) {
+//    input = input.split("\n")
+//    let arr = input[0].trim().split(" ").map(Number)
     
-// console.log(input)
-let maximum = -Infinity
+// console.log(arr)
+// let maximum = -Infinity
+// let sum;
+// for(let i=0;i<arr.length;i++){
+//    sum = (arr[i]+arr[i+1]+arr[i+2])
+//     if(sum > maximum ){
+//         maximum == sum
+//     }
+//     //console.log(sum)
+//     console.log(maximum)
+// }
 
-for(let i=0;i<input.length;i++){
-    if((input[i]+input[i+1]+input[i+2]) > maximum ){
-        maximum == input[i]+input[i+1]+input[i+2]
-    }
-}
 
-console.log(maximum)
     
-}
+// }
  
 
-   if (process.env.USERNAME === 'haris') {
-     runProgram(`2 0 1 100 200 10 7 2 300 2 10`)
-    } else {
-     process.stdin.resume();
-     process.stdin.setEncoding("ascii");
-     let read = "";
-     process.stdin.on("data", function (input) {
-       read += input;
-     });
-     process.stdin.on("end", function () {
-       read = read.replace(/\n$/, "");
-       read = read.replace(/\n$/, "");
-       runProgram(read);
-     });
-     process.on("SIGINT", function () {
-       read = read.replace(/\n$/, "");
-       runProgram(read);
-       process.exit(0);
-     });
-   }
+//    if (process.env.USERNAME === 'haris') {
+//      runProgram(`2 0 1 100 200 10 7 2 300 2 10`)
+//     } else {
+//      process.stdin.resume();
+//      process.stdin.setEncoding("ascii");
+//      let read = "";
+//      process.stdin.on("data", function (input) {
+//        read += input;
+//      });
+//      process.stdin.on("end", function () {
+//        read = read.replace(/\n$/, "");
+//        read = read.replace(/\n$/, "");
+//        runProgram(read);
+//      });
+//      process.on("SIGINT", function () {
+//        read = read.replace(/\n$/, "");
+//        runProgram(read);
+//        process.exit(0);
+//      });
+//    }
    

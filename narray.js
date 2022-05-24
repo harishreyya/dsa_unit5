756d9a05d17302c269422ff1b8a41d3440bde609
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-05-24 19:41:50 +0530

    comment

--- narray.js
+++ narray.js
@@ -1,30 +1,30 @@
 
-function runProgram(input) {
+// function runProgram(input) {
    
     
 
     
-}
+// }
  
 
-   if (process.env.USERNAME === 'haris') {
-     runProgram(``)
-    } else {
-     process.stdin.resume();
-     process.stdin.setEncoding("ascii");
-     let read = "";
-     process.stdin.on("data", function (input) {
-       read += input;
-     });
-     process.stdin.on("end", function () {
-       read = read.replace(/\n$/, "");
-       read = read.replace(/\n$/, "");
-       runProgram(read);
-     });
-     process.on("SIGINT", function () {
-       read = read.replace(/\n$/, "");
-       runProgram(read);
-       process.exit(0);
-     });
-   }
+//    if (process.env.USERNAME === 'haris') {
+//      runProgram(``)
+//     } else {
+//      process.stdin.resume();
+//      process.stdin.setEncoding("ascii");
+//      let read = "";
+//      process.stdin.on("data", function (input) {
+//        read += input;
+//      });
+//      process.stdin.on("end", function () {
+//        read = read.replace(/\n$/, "");
+//        read = read.replace(/\n$/, "");
+//        runProgram(read);
+//      });
+//      process.on("SIGINT", function () {
+//        read = read.replace(/\n$/, "");
+//        runProgram(read);
+//        process.exit(0);
+//      });
+//    }
    

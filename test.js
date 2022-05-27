ce585f8569e393250f0544a464fd7f3379a4104b
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-05-27 12:49:20 +0530

    argd

--- test.js
+++ test.js
@@ -302,3 +302,30 @@
 //everything which not in the function is in the global execution
 
 // ------------------------------------------------------------------------
+
+// var array = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
+
+// ; // [1, 2, 3, 5, 9, 8]
+
+// console.log(Array.from(new Set(array)))
+
+
+
+// var array = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
+
+// console.log(uniqueArray(array)); // [1, 2, 3, 5, 9, 8]
+
+// function uniqueArray(array) {
+//   var hashmap = {};
+//   var unique = [];
+
+//   for(var i = 0; i < array.length; i++) {
+//     // If key returns undefined (unique), it is evaluated as false.
+//     if(!hashmap.hasOwnProperty(array[i])) {
+//       hashmap[array[i]] = 1;
+//       unique.push(array[i]);
+//     }
+//   }
+
+//   return unique;
+// }

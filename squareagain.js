d5d9e6ae269e0012c28f3188e6e6409a0b74af56
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-07-21 18:56:45 +0530

    htcdhry

--- squareagain.js
+++ squareagain.js
@@ -1,26 +1,26 @@
-function runProgram(input) {
-    let n = input.trim().split("")
+// function runProgram(input) {
+//     let n = input.trim().split("")
     
-}
+// }
    
-   if (process.env.USERNAME === 'haris') {
-     runProgram(`4`);
-   } else {
-     process.stdin.resume();
-     process.stdin.setEncoding("ascii");
-     let read = "";
-     process.stdin.on("data", function (input) {
-       read += input;
-     });
-     process.stdin.on("end", function () {
-       read = read.replace(/\n$/, "");
-       read = read.replace(/\n$/, "");
-       runProgram(read);
-     });
-     process.on("SIGINT", function () {
-       read = read.replace(/\n$/, "");
-       runProgram(read);
-       process.exit(0);
-     });
-   }
+//    if (process.env.USERNAME === 'haris') {
+//      runProgram(`4`);
+//    } else {
+//      process.stdin.resume();
+//      process.stdin.setEncoding("ascii");
+//      let read = "";
+//      process.stdin.on("data", function (input) {
+//        read += input;
+//      });
+//      process.stdin.on("end", function () {
+//        read = read.replace(/\n$/, "");
+//        read = read.replace(/\n$/, "");
+//        runProgram(read);
+//      });
+//      process.on("SIGINT", function () {
+//        read = read.replace(/\n$/, "");
+//        runProgram(read);
+//        process.exit(0);
+//      });
+//    }
    

f2b891a7cb803dec6d5649beb0f96dd5424a9233
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-08-10 18:02:55 +0530

    Etb

--- testing/swe.js
+++ testing/swe.js
@@ -1,11 +1,27 @@
-let i = 1;
+// let i = 1;
 
-console.log(i++)
-console.log(i++)
-let j = i++ - 2;
-let arr = [6,2,7,3,5]
-for(i=0;i<arr.length;i++){
-    if(arr[i]> arr[i+1]){
-        arr[i]=arr[i+1]
-    }
-}
+// console.log(i++)
+// console.log(i++)
+// let j = i++ - 2;
+// let arr = [6,2,7,3,5]
+// for(i=0;i<arr.length;i++){
+//     if(arr[i]> arr[i+1]){
+//         arr[i]=arr[i+1]
+//     }
+// }
+let arr = [1,2,3]
+console.log(typeof(arr))
+
+let str = "jtdutc"
+console.log(typeof(str))
+
+let num = 54
+console.log(typeof(num))
+console.log(typeof(null))
+console.log(typeof(undefined))
+console.log(typeof(Number))
+console.log(typeof(Array))
+console.log(typeof(Object))
+console.log(typeof(String))
+
+console.log(typeof())

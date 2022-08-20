81ad3b5f8a44f8660a9c36160d6eee251efaaca5
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-08-20 23:37:23 +0530

    wef

--- testing/swe.js
+++ testing/swe.js
@@ -1,35 +1,35 @@
-// // let i = 1;
+// let i = 1;
 
-// // console.log(i++)
-// // console.log(i++)
-// // let j = i++ - 2;
-// // let arr = [6,2,7,3,5]
-// // for(i=0;i<arr.length;i++){
-// //     if(arr[i]> arr[i+1]){
-// //         arr[i]=arr[i+1]
-// //     }
-// // }
+// console.log(i++)
+// console.log(i++)
+// let j = i++ - 2;
+// let arr = [6,2,7,3,5]
+// for(i=0;i<arr.length;i++){
+//     if(arr[i]> arr[i+1]){
+//         arr[i]=arr[i+1]
+//     }
+// }
 // let arr = [1,2,3]
 // console.log(typeof(arr))
 
 // let str = "jtdutc"
 // console.log(typeof(str))
 
-// let num = 54
-// console.log(typeof(num))
-// console.log(typeof(null))
-// console.log(typeof(undefined))
-// console.log(typeof(Number))
-// console.log(typeof(Array))
-// // console.log(typeof(Object))
-// // console.log(typeof(String))
+let num = 54
+console.log(typeof(num))
+console.log(typeof(null))
+console.log(typeof(undefined))
+console.log(typeof(Number))
+console.log(typeof(Array))
+// console.log(typeof(Object))
+// console.log(typeof(String))
 
-// let arr = [1,2,3,4,5,6,7];
-// arr.shift()
-// console.log(arr)
-// arr.unshift("htc")
-// console.log(arr)
-// let str = arr.toString()
-// console.log(str)
-// // let rearr = str.toArray()
-// console.log(rearr)
+let arr = [1,2,3,4,5,6,7];
+arr.shift()
+console.log(arr)
+arr.unshift("htc")
+console.log(arr)
+let str = arr.toString()
+console.log(str)
+// let rearr = str.toArray()
+console.log(rearr)

11a208fd939a9ca24e3fdd27050d943ad3d843c5
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-09-15 22:31:10 +0530

    sr

---
+++ ccc.js
@@ -0,0 +1,35 @@
+
+// function runProgram(input) {
+//     input = input.trim().split("\n");
+
+
+
+
+// } 
+//    if (process.env.USERNAME === 'haris') {
+//      runProgram(`4
+//      SSSSEEEECCCCEECCCC
+//      CCCCCSSSSEEECCCCSS
+//      SSSSSEEESSCCCCCCCS
+//      EESSSSCCCCCCSSEEEE`)
+//     } else {
+//      process.stdin.resume();
+//      process.stdin.setEncoding("ascii");
+//      let read = "";
+//      process.stdin.on("data", function (input) {
+//        read += input;
+//      });
+//      process.stdin.on("end", function () {
+//        read = read.replace(/\n$/, "");
+//        read = read.replace(/\n$/, "");
+//        runProgram(read);
+//      });
+//      process.on("SIGINT", function () {
+//        read = read.replace(/\n$/, "");
+//        runProgram(read);
+//        process.exit(0);
+//      });
+//    }
+   
+
+   

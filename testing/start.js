053c9083d950a9da50beaf3564e9cc307595b256
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2022-11-01 19:12:42 +0530

    gfrds

--- testing/start.js
+++ testing/start.js
@@ -1,29 +1,31 @@
-// function isletter(input){
-//     return input.match(/[a-z]/i);
-//         }
-// function runProgram(input) {
-//     input = input.trim().split(" ");
-//   console.log( isletter(input)) 
-   
-// }
-//    if (process.env.USERNAME === 'haris') {
-//      runProgram(`1 2 3 4 j 4 5 6`)
-//     } else {
-//      process.stdin.resume();
-//      process.stdin.setEncoding("ascii");
-//      let read = "";
-//      process.stdin.on("data", function (input) {
-//        read += input;
-//      });
-//      process.stdin.on("end", function () {
-//        read = read.replace(/\n$/, "");
-//        read = read.replace(/\n$/, "");
-//        runProgram(read);
-//      });
-//      process.on("SIGINT", function () {
-//        read = read.replace(/\n$/, "");
-//        runProgram(read);
-//        process.exit(0);
-//      });
-//    }
+
+function runProgram(input) {
+    input = input.trim().split(" ").map(Number);
+    console.log(input);
+    let a=0;
+   for(let i of input){
+    a+=i;
+   }
+   console.log(a)
+}
+   if (process.env.USERNAME === 'haris') {
+     runProgram(`1 2 3 4 5 6`)
+    } else {
+     process.stdin.resume();
+     process.stdin.setEncoding("ascii");
+     let read = "";
+     process.stdin.on("data", function (input) {
+       read += input;
+     });
+     process.stdin.on("end", function () {
+       read = read.replace(/\n$/, "");
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+     });
+     process.on("SIGINT", function () {
+       read = read.replace(/\n$/, "");
+       runProgram(read);
+       process.exit(0);
+     });
+   }
    

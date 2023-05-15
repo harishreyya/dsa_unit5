9bd4e819d3ac5521a62886f563aa8e60542749d6
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2023-05-15 10:37:47 +0530

    srg

--- testing/java.js
+++ testing/java.js
@@ -43,3 +43,18 @@
 // function b(){
 
 // }
+
+const arrayOfColorFunctions = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+
+// let randomColorString = '#';
+let randomColorString = '#';
+
+
+for (let x = 0; x < 6; x++){
+
+    let index = Math.floor(Math.random() * 16)
+    let value = arrayOfColorFunctions[index]
+
+    randomColorString += value
+}
+console.log(randomColorString)

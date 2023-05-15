4e449872ff8ef31027c3d7b3968dba4f028a5363
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2023-05-15 13:41:01 +0530

    sfv

--- 2darray.js
+++ 2darray.js
@@ -15,10 +15,39 @@
 
 // console.log(sum)
 
-const arr = [1,2,3,4,5,6,7,8,9];
+// const arr = [1,2,3,4,5,6,7,8,9];
     
-const newArr = [];
-while(arr.length)
- newArr.push(arr.splice(0,3));
+// const newArr = [];
+// while(arr.length)
+//  newArr.push(arr.splice(0,3));
     
-console.log(newArr);
+// console.log(newArr);
+
+var cubes = [
+        ['color', 100, true],
+        ['red', 200, false],    
+        ['white', 500, true],
+       ];
+result = [];
+boolean = [];
+str = []
+
+       arr = [].concat(...cubes)
+    //    console.log(arr)
+       for(i=0;i<arr.length;i++){
+        if(typeof(arr[i])=='number'){
+   result.push(arr[i])
+        }else if(typeof(arr[i])=='boolean'){
+boolean.push(arr[i])
+        }else{
+   str.push(arr[i])
+        }
+       }
+
+    ans = [];
+    ans.push(str)
+    ans.push(result)
+    ans.push(boolean)
+
+    console.log(cubes)
+       console.log(ans)

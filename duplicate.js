ee6b47aa0c5765efd6bdead42e49f1386cf82f6a
Harish Reyya <87423101+harishreyya@users.noreply.github.com>
2023-05-15 11:07:43 +0530

    dd

---
+++ duplicate.js
@@ -0,0 +1,29 @@
+function runProgram(input) {
+    input = input.trim().split(" ");
+    input = [...new Set(input)]
+    console.log(input.join(" "))
+}
+
+
+   
+
+  if (process.env.USERNAME === "haris") {
+    runProgram(`3 3 5 5 1`);
+  } else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+      read += input;
+    });
+    process.stdin.on("end", function () {
+      read = read.replace(/\n$/, "");
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+    });
+    process.on("SIGINT", function () {
+      read = read.replace(/\n$/, "");
+      runProgram(read);
+      process.exit(0);
+    });
+  }
